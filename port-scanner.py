869d609a25ec32261af915360e4c767064ce19c3
Ayush Chourasia <76032725+sudo-Ayush@users.noreply.github.com>
2022-09-24 20:52:48 +0530

    Update port-scanner.py

--- port-scanner.py
+++ port-scanner.py
@@ -27,53 +27,62 @@ else:
     _ = system('clear')
 
 print(f"{Fore.YELLOW}[*] Enter the domain name without <http> or <https> or simply enter IP Address...")
-print(f"{Fore.YELLOW}[*] Example = github.com or 20.207.73.82")
+print(f"{Fore.YELLOW}[*] Example => github.com or 20.207.73.82")
 print(f"{Fore.LIGHTCYAN_EX}--------------------------")
 name = input(f"{Fore.BLUE}[*] Enter IP : ").strip()
-ip = socket.gethostbyname(name)
 
-if validation(ip) == True:
-    rs = input(f"{Fore.BLUE}[*] Do you want to use defult ports scan[Y/N]: ").upper()
+try :
+    ip = socket.gethostbyname(name)
+    if validation(ip) == True:
+        rs = input(f"{Fore.BLUE}[*] Do you want to use defult ports scan[Y/N]: ").upper()
 
-    if rs == "N":
-        print(f"{Fore.LIGHTCYAN_EX}-----------------"+ "-" * len(ip))
-        print(f"{Fore.LIGHTRED_EX}[*] Target IP : {ip}")
-        print(f"{Fore.LIGHTCYAN_EX}-----------------"+ "-" * len(ip))
+        if rs == "N":
+            print(f"{Fore.LIGHTCYAN_EX}-----------------"+ "-" * len(ip))
+            print(f"{Fore.LIGHTRED_EX}[*] Target IP : {ip}")
+            print(f"{Fore.LIGHTCYAN_EX}-----------------"+ "-" * len(ip))
 
-        start = int(input(f"{Fore.LIGHTBLUE_EX}[*] Enter start point : "))
-        end = int(input(f"{Fore.LIGHTBLUE_EX}[*] Enter end point : "))
-        print(f"{Fore.LIGHTMAGENTA_EX}-----------------"+ "-" * len(ip))
-        ts = time.time()
-                
-        for port in range(start,end+1):
-            time.sleep(0.009)
-            thread = threading.Thread(target = port_scan, args = (ip, port))
-            thread.start()
-            
-    elif rs == "Y":
-        print(f"{Fore.LIGHTCYAN_EX}-----------------"+ "-" * len(ip))
-        print(f"{Fore.LIGHTRED_EX}[*] Target IP : {ip}")
-        print(f"{Fore.LIGHTCYAN_EX}-----------------"+ "-" * len(ip))
-        
-        l = [20,21,22,23,25,53,67,68,69,70,79,80,88,105,106,110,123,139,143,144,161,
-            389,443,445,465,520,554,587,666,993,995,2222,2224,3360,3389,8000,8080]
-
-        ts = time.time()
+            start = int(input(f"{Fore.LIGHTBLUE_EX}[*] Enter start point : "))
+            end = int(input(f"{Fore.LIGHTBLUE_EX}[*] Enter end point : "))
+            print(f"{Fore.LIGHTMAGENTA_EX}-----------------"+ "-" * len(ip))
+            ts = time.time()
                     
-        for port in l:
-            time.sleep(0.009)
-            thread = threading.Thread(target = port_scan, args = (ip, port))
-            thread.start()
+            for port in range(start,end+1):
+                time.sleep(0.009)
+                thread = threading.Thread(target = port_scan, args = (ip, port))
+                thread.start()
+                
+        elif rs == "Y":
+            print(f"{Fore.LIGHTCYAN_EX}-----------------"+ "-" * len(ip))
+            print(f"{Fore.LIGHTRED_EX}[*] Target IP : {ip}")
+            print(f"{Fore.LIGHTCYAN_EX}-----------------"+ "-" * len(ip))
+            
+            l = [20,21,22,23,25,53,67,68,69,70,79,80,88,105,106,110,123,139,143,144,161,
+                389,443,445,465,520,554,587,666,993,995,2222,2224,3360,3389,8000,8080]
+
+            ts = time.time()
+                        
+            for port in l:
+                time.sleep(0.009)
+                thread = threading.Thread(target = port_scan, args = (ip, port))
+                thread.start()
+        else:
+            print(f"{Fore.RED}[-] Invalid Selection...!")
+            exit()
+
+        done = (time.time()-ts)
+
+        time.sleep(1)
+        print(f"{Fore.LIGHTCYAN_EX}-------------------------"+ "-" * len(ip))
+        print(f'{Fore.LIGHTMAGENTA_EX}[*] Finished | Scanned in {round(done,2)} seconds')
+    
     else:
-        print(f"{Fore.RED}[-] Invalid Selection...!")
+        print(f"{Fore.RED}[-] Invalid IP Address...!")
         exit()
 
-    done = (time.time()-ts)
-
-    time.sleep(1)
-    print(f"{Fore.LIGHTCYAN_EX}-------------------------"+ "-" * len(ip))
-    print(f'{Fore.LIGHTMAGENTA_EX}[*] Finished | Scanned in {round(done,2)} seconds')
+except socket.gaierror:
+    print(f"{Fore.RED}[-] Error : Invalid domain or IP...!")
     exit()
-   
-else:
-    print(f"{Fore.RED}[-] Invalid IP Address...!")
+    
+except KeyboardInterrupt:
+    print(f"{Fore.RED}[-] You press [Ctrl + C]\nExiting...")
+    exit()
